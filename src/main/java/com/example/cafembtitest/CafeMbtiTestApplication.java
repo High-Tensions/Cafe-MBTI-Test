32d3fc2bbd129bb09544a82fc180a43e7e064d8c
kyj4528 <kyj4528@naver.com>
2025-02-12 17:05:39 +0900

    project setting

---
+++ src/main/java/com/example/cafembtitest/CafeMbtiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafembtitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMbtiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeMbtiTestApplication.class, args);
+    }
+
+}
